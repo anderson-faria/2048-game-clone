11a372782b4373bf49016309e2693fedad5fa913
Anderson Faria <40532297+anderson-faria@users.noreply.github.com>
2020-02-12 23:52:36 -0300

    Version 0.1
    - Player can move blocks to left and right;
    - Player can join blocks with the same number;
    - Blocks change color and size of font according to number in it;
    - Blocks are added to the matrix after player makes moves;
    - An animation plays when blocks with same number joins;
    - Two blocks are added at the start (2 and 2, 4 and 2 or 2 and 4, never 4 and 4).

---
+++ Assets/Block.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Block : MonoBehaviour
+{
+    private Image blockColor;
+    private TextMeshProUGUI blockText;
+    private TextMeshProUGUI blockTextColor;
+
+    void Start()
+    {
+        blockColor = gameObject.GetComponent<Image>();
+
+        blockText = gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+
+        blockTextColor = gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        switch(blockText.text) // ADAPT THE BACKGROUND COLOR OF THE BLOCK
+        {
+            case "":
+                blockColor.color = new Color32(0x34, 0x3A, 0x4C, 0xFF);
+                break;
+
+            case "2":
+                blockColor.color = new Color32(0x3B, 0x73, 0xEB, 0xFF);
+                break;
+
+            case "4":
+                blockColor.color = new Color32(0xFF, 0xE5, 0x59, 0xFF);
+                break;
+
+            case "8":
+                blockColor.color = new Color32(0xDF, 0x75, 0x45, 0xFF);
+                break;
+
+            case "16":
+                blockColor.color = new Color32(0x7F, 0x40, 0xC2, 0xFF);
+                break;
+
+            case "32":
+                blockColor.color = new Color32(0x69, 0xCA, 0x69, 0xFF);
+                break;
+
+            case "64":
+                blockColor.color = new Color32(0xA9, 0xDA, 0x4F, 0xFF);
+                break;
+
+            case "128":
+                blockColor.color = new Color32(0x39, 0x5F, 0xCD, 0xFF);
+                break;
+
+            case "256":
+                blockColor.color = new Color32(0x39, 0xB3, 0xCD, 0xFF);
+                break;
+
+            case "512":
+                blockColor.color = new Color32(0xC1, 0x63, 0xDE, 0xFF);
+                break;
+
+            case "1024":
+                blockColor.color = new Color32(0xFF, 0x6B, 0x90, 0xFF);
+                break;
+
+            case "2048":
+                blockColor.color = new Color32(0xFF, 0x6B, 0x5B, 0xFF);
+                break;
+        }
+
+        switch(blockText.text.Length) // ADAPT FONT SIZE
+        {
+            case 1:
+                blockText.fontSize = 90;
+                break;
+
+            case 2:
+                blockText.fontSize = 78;
+                break;
+
+            case 3:
+                blockText.fontSize = 65;
+                break;
+
+            case 4:
+                blockText.fontSize = 50;
+                break;
+        }
+    }
+}
---
+++ Assets/Matrix.cs
@@ -0,0 +1,303 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Matrix : MonoBehaviour
+{
+    private int[,] matrix = new int[4, 4] {
+        { 0, 0, 0, 0 },
+        { 0, 0, 0, 0 },
+        { 0, 0, 0, 0 },
+        { 0, 0, 0, 0 }
+    };
+
+    private int posX = 0;
+    private int posY = 0;
+    private int value = 2;
+    private int emptySpaces = 16;
+
+    public TextMeshProUGUI[] line0 = new TextMeshProUGUI[4];
+    public TextMeshProUGUI[] line1 = new TextMeshProUGUI[4];
+    public TextMeshProUGUI[] line2 = new TextMeshProUGUI[4];
+    public TextMeshProUGUI[] line3 = new TextMeshProUGUI[4];
+
+    void Start()
+    {
+        GenerateBlock();
+        GenerateBlock();
+        ChangeMatrix();
+    }
+
+    void Update()
+    {
+        SearchEmptySpaces();
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GenerateBlock();
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            MoveLeft();
+            JoinBlocksLeft();
+            MoveLeft();
+            GenerateBlock();
+            ChangeMatrix();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            MoveRight();
+            JoinBlocksRight();
+            MoveRight();
+            GenerateBlock();
+            ChangeMatrix();
+        } 
+    }
+
+    private void ChangeMatrix()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (matrix[0, i] == 0)
+            {
+                line0[i].transform.GetComponent<TextMeshProUGUI>().text = "";
+            }
+            else
+            {
+                line0[i].transform.GetComponent<TextMeshProUGUI>().text = matrix[0, i].ToString();
+            }
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (matrix[1, i] == 0)
+            {
+                line1[i].transform.GetComponent<TextMeshProUGUI>().text = "";
+            }
+            else
+            {
+                line1[i].transform.GetComponent<TextMeshProUGUI>().text = matrix[1, i].ToString();
+            }
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (matrix[2, i] == 0)
+            {
+                line2[i].transform.GetComponent<TextMeshProUGUI>().text = "";
+            }
+            else
+            {
+                line2[i].transform.GetComponent<TextMeshProUGUI>().text = matrix[2, i].ToString();
+            }
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            if (matrix[3, i] == 0)
+            {
+                line3[i].transform.GetComponent<TextMeshProUGUI>().text = "";
+            }
+            else
+            {
+                line3[i].transform.GetComponent<TextMeshProUGUI>().text = matrix[3, i].ToString();
+            }
+        }
+    }
+
+    private void PrintMatrix()
+    {
+        Debug.Log("\n" +
+            matrix[0, 0] + " " + matrix[0, 1] + " " + matrix[0, 2] + " " + matrix[0, 3] + "\n" +
+            matrix[1, 0] + " " + matrix[1, 1] + " " + matrix[1, 2] + " " + matrix[1, 3] + "\n" +
+            matrix[2, 0] + " " + matrix[2, 1] + " " + matrix[2, 2] + " " + matrix[2, 3] + "\n" +
+            matrix[3, 0] + " " + matrix[3, 1] + " " + matrix[3, 2] + " " + matrix[3, 3] + "\n"
+        );
+    }
+
+    private void GenerateBlock()
+    {
+        if(emptySpaces > 0)
+        {
+            bool okay = false;
+            posX = Random.Range(0, 4);
+            posY = Random.Range(0, 4);
+
+            while (!okay) // IMPEDIR QUE GERE 4 E 4 !!!!!!!
+            {
+                if (matrix[posY, posX] == 0)
+                {
+                    if(emptySpaces == 15 && value == 4)
+                    {
+                        value = 2;
+                    }
+                    else
+                    {
+                        if (Random.Range(1, 28) % 2 == 0)
+                        {
+                            value = 2;
+                        }
+                        else
+                        {
+                            value = 4;
+                        }
+                    }
+                    matrix[posY, posX] = value;
+                    okay = true;
+                }
+                else
+                {
+                    posX = Random.Range(0, 4);
+                    posY = Random.Range(0, 4);
+                }
+            }
+        }
+        
+    }
+
+    private void SearchEmptySpaces()
+    {
+        emptySpaces = 0;
+
+        for(int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                if(matrix[i, j] == 0)
+                {
+                    emptySpaces++;
+                }
+            }
+        }
+    }
+
+    private void ExecuteJoinAnimation(int i, int j)
+    {
+        int childIndex = 0;
+        Animation anim;
+
+        switch (i)
+        {
+            case 0:
+                childIndex = 0 + j;
+                break;
+
+            case 1:
+                childIndex = 4 + j;
+                break;
+
+            case 2:
+                childIndex = 8 + j;
+                break;
+
+            case 3:
+                childIndex = 12 + j;
+                break;
+        }
+
+        anim = this.gameObject.transform.GetChild(childIndex).transform.GetComponent<Animation>();
+        anim.Play("JoinBlock");
+    }
+
+    private void MoveLeft()
+    {
+        for(int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                if(matrix[i, j] == 0 && j != 3)
+                {
+                    for (int k = j + 1; k < 4; k++)
+                    {
+                        if(matrix[i, k] != 0)
+                        {
+                            matrix[i, j] = matrix[i, k];
+                            matrix[i, k] = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void JoinBlocksLeft()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                if(matrix[i, j] != 0 && j != 3)
+                {
+                    if (matrix[i, j] == matrix[i, j + 1])
+                    {
+                        matrix[i, j] *= 2;
+
+                        ExecuteJoinAnimation(i, j);
+
+                        matrix[i, j + 1] = 0;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    private void MoveRight()
+    {
+        for (int i = 3; i >= 0; i--)
+        {
+            for (int j = 3; j >= 0; j--)
+            {
+                if (matrix[i, j] == 0 && j != 0)
+                {
+                    for (int k = j - 1; k >= 0; k--)
+                    {
+                        if (matrix[i, k] != 0)
+                        {
+                            matrix[i, j] = matrix[i, k];
+                            matrix[i, k] = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void JoinBlocksRight()
+    {
+        for (int i = 3; i >= 0; i--)
+        {
+            for (int j = 3; j >= 0; j--)
+            {
+                if (matrix[i, j] != 0 && j != 0)
+                {
+                    if (matrix[i, j] == matrix[i, j - 1])
+                    {
+                        matrix[i, j] *= 2;
+
+                        ExecuteJoinAnimation(i, j);
+
+                        matrix[i, j - 1] = 0;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    private void MoveUp()
+    {
+        
+    }
+
+    private void JoinBlocksUp()
+    {
+        
+    }
+}
